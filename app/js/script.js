bac762fbe4cd3f712a4777e41b0fdca00b3222fa
Kytario <96780163+Kytario@users.noreply.github.com>
2023-09-11 22:22:35 +0300

    Initial commit

---
+++ app/js/script.js
@@ -0,0 +1,17 @@
+let x = Math.floor(Math.random() * 100) + 1;
+document.getElementById("result").innerHTML = x;
+
+let y = Math.floor(Math.random() * 100) + 1;
+document.getElementById("procent").innerHTML = y;
+
+let g = Math.floor(Math.random() * 100) + 1;
+document.getElementById("reaction").innerHTML = g;
+
+let d = Math.floor(Math.random() * 100) + 1;
+document.getElementById("memory").innerHTML = d;
+
+let a = Math.floor(Math.random() * 100) + 1;
+document.getElementById("verbal").innerHTML = a;
+
+let t = Math.floor(Math.random() * 100) + 1;
+document.getElementById("visual").innerHTML = t;
